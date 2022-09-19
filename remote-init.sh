2aabd906d9ddc5f68c615d3673e2066f9856c711
Chris Jordan <cmjordan@gmail.com>
2022-09-19 17:28:52 -0400

    v1.1 restore settings from existing a-hole instance

--- remote-init.sh
+++ remote-init.sh
@@ -7,6 +7,7 @@ DDNS=$3
 DDNSPASS=$4
 PIPASS=$5
 PEERS=$6
+COPYFROMDDNS=$7
 
 if [ -z "$IP" ] || [ -z "$EMAIL" ] || [ -z "$DDNS" ] || [ -z "$DDNSPASS" ] || [ -z "$PIPASS" ] || [ -z "$PEERS" ]; then
     echo "missing parameters: remote-init.sh <IP> <email address> <DDNS> <DDNS password> <password to set for Pihole> <list of Wireguard peer names>"
@@ -47,6 +48,20 @@ ssh -oStrictHostKeyChecking=no ubuntu@${IP} sudo systemctl enable containerd.ser
 
 echo "--- A-HOLE schedule host machine to reboot once a week on Wednesday morning"
 ssh -oStrictHostKeyChecking=no ubuntu@${IP} sudo crontab -l | grep -q 'shutdown -r' && echo 'a scheduled reboot entry exists' || (crontab -l 2>/dev/null; echo "0 7 * * 3 /sbin/shutdown -r") | crontab -
+
+if [ -z "$COPYFROMDDNS" ]; then
+    echo "--- A-HOLE no DDNS specified to restore from; continuing"
+else
+    echo "--- A-HOLE restoring metadata from ${COPYFROMDDNS}"
+    mkdir -p ./tmp
+    scp -r ubuntu@${COPYFROMDDNS}:~/pihole-etc-dnsmasq.d ./tmp/
+    scp -r ./tmp/pihole-etc-dnsmasq.d ubuntu@${IP}:~/
+    scp -r ubuntu@${COPYFROMDDNS}:~/pihole-etc-pihole ./tmp/
+    scp -r ./tmp/pihole-etc-pihole ubuntu@${IP}:~/
+    scp -r ubuntu@${COPYFROMDDNS}:~/wireguard-config ./tmp/
+    scp -r ./tmp/wireguard-config ubuntu@${IP}:~/
+    rm -rf ./tmp
+fi    
 
 # bring up the platform
 echo "--- A-HOLE bringing up platform"
