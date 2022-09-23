aa56e327f213e7f54035927d7f27b25ee6a3d137
Chris Jordan <cmjordan@gmail.com>
2022-09-23 17:49:54 -0400

    220923 move back to local init script instead of ssh calls

--- remote-init.sh
+++ remote-init.sh
@@ -26,28 +26,8 @@ sed -i "s/PEERS\!\!\!/$PEERS/g" ./${DDNS}/*
 echo "--- A-HOLE copying files to target host"
 scp -oStrictHostKeyChecking=no -r ./${DDNS}/* ubuntu@${IP}:~/
 
-echo "--- A-HOLE basic updates for the host machine"
-ssh -oStrictHostKeyChecking=no ubuntu@${IP} sudo DEBIAN_FRONTEND=noninteractive apt --yes --allow-change-held-packages update
-ssh -oStrictHostKeyChecking=no ubuntu@${IP} sudo DEBIAN_FRONTEND=noninteractive apt --yes --allow-change-held-packages upgrade
-ssh -oStrictHostKeyChecking=no ubuntu@${IP} sudo DEBIAN_FRONTEND=noninteractive apt --yes --allow-change-held-packages autoremove
-ssh -oStrictHostKeyChecking=no ubuntu@${IP} sudo DEBIAN_FRONTEND=noninteractive apt --yes --allow-change-held-packages autoclean
-
-echo "--- A-HOLE install dependent packages"
-ssh -oStrictHostKeyChecking=no ubuntu@${IP} sudo DEBIAN_FRONTEND=noninteractive apt install --yes --allow-change-held-packages python3 docker.io docker-compose
-
-echo "--- A-HOLE configure host machine for unattended upgrades"
-ssh -oStrictHostKeyChecking=no ubuntu@${IP} sudo DEBIAN_FRONTEND=noninteractive apt install --yes --allow-change-held-packages unattended-upgrades apt-listchanges bsd-mailx
-ssh -oStrictHostKeyChecking=no ubuntu@${IP} sudo dpkg-reconfigure -fnoninteractive -plow unattended-upgrades
-
-echo "--- A-HOLE add user to docker group to avoid sudo"
-ssh -oStrictHostKeyChecking=no ubuntu@${IP} sudo usermod -aG docker ubuntu
-
-echo "--- A-HOLE configure docker to start on machine reboot"
-ssh -oStrictHostKeyChecking=no ubuntu@${IP} sudo systemctl enable docker.service
-ssh -oStrictHostKeyChecking=no ubuntu@${IP} sudo systemctl enable containerd.service
-
-echo "--- A-HOLE schedule host machine to reboot once a week on Wednesday morning"
-ssh -oStrictHostKeyChecking=no ubuntu@${IP} sudo crontab -l | grep -q 'shutdown -r' && echo 'a scheduled reboot entry exists' || (crontab -l 2>/dev/null; echo "0 7 * * 3 /sbin/shutdown -r") | crontab -
+echo "--- A-HOLE running local init"
+ssh -oStrictHostKeyChecking=no ubuntu@${IP} ./local-init.sh
 
 if [ -z "$COPYFROMDDNS" ]; then
     echo "--- A-HOLE no DDNS specified to restore from; continuing"
---
+++ template/local_init.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "--- A-HOLE basic updates for the host machine"
+sudo DEBIAN_FRONTEND=noninteractive apt --yes --allow-change-held-packages update
+sudo DEBIAN_FRONTEND=noninteractive apt --yes --allow-change-held-packages upgrade
+sudo DEBIAN_FRONTEND=noninteractive apt --yes --allow-change-held-packages autoremove
+sudo DEBIAN_FRONTEND=noninteractive apt --yes --allow-change-held-packages autoclean
+
+echo "--- A-HOLE install dependent packages"
+sudo DEBIAN_FRONTEND=noninteractive apt install --yes --allow-change-held-packages python3 docker.io docker-compose
+
+echo "--- A-HOLE configure host machine for unattended upgrades"
+sudo DEBIAN_FRONTEND=noninteractive apt install --yes --allow-change-held-packages unattended-upgrades apt-listchanges bsd-mailx
+sudo dpkg-reconfigure -fnoninteractive -plow unattended-upgrades
+
+echo "--- A-HOLE add user to docker group to avoid sudo"
+sudo usermod -aG docker ubuntu
+
+echo "--- A-HOLE configure docker to start on machine reboot"
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+echo "--- A-HOLE schedule host machine to reboot once a week on Wednesday morning"
+sudo crontab -l | grep -q 'shutdown -r' && echo 'a scheduled reboot entry exists' || (crontab -l 2>/dev/null; echo "0 7 * * 3 /sbin/shutdown -r") | crontab -
