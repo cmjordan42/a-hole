c549e5878d9d1051feb3d8017224630a1388c982
Chris Jordan <cmjordan@gmail.com>
2025-03-31 14:51:54 -0400

    move duckdns update to cron curl

--- template/init-ahole.sh
+++ template/init-ahole.sh
@@ -20,5 +20,14 @@ echo "--- A-HOLE configure docker to start on machine reboot"
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 
-echo "--- A-HOLE schedule host machine to reboot once a week on Wednesday morning at 4:00"
-sudo crontab -l | grep 'reboot' && echo '--- MYFI a scheduled reboot exists in crontab' || (sudo crontab -l 2>/dev/null; echo "0 5 * * 0 sudo /usr/sbin/reboot") | sudo crontab -
+echo "--- A-HOLE setting timezone"
+sudo timedatectl set-timezone America/Detroit
+
+echo "--- A-HOLE sudo schedule host machine to reboot once a week on Wednesday morning at 6:00"
+sudo crontab -l | grep 'reboot' && echo '--- A-HOLE reboot in crontab' || (sudo crontab -l 2>/dev/null; echo "0 4 * * 6 /usr/sbin/reboot") | sudo crontab -
+
+echo "--- A-HOLE schedule certbot to run once a week on Wednesday morning at 6:10"
+crontab -l | grep 'certbot' && echo '--- A-HOLE certbot in crontab' || (crontab -l 2>/dev/null; echo "10 4 * * 6 /usr/bin/docker-compose -f ~/a-hole/docker-compose.yml up certbot") | crontab -
+
+echo "--- A-HOLE schedule duckdns IP update hourly at 5 minutes past the hour"
+crontab -l | grep 'cjpi.duckdns' && echo '--- A-HOLE duckdns in crontab' || (crontab -l 2>/dev/null; echo "5 * * * * curl -L \"https://www.duckdns.org/update?domains=DDNS!!!&token=DDNSPASS!!!&verbose=true\"") | crontab -
